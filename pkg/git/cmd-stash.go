0c430a1e638be37e0afb9ef3759971a0ab0cd132
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2018-12-28 01:21:11 +0300

    fixes a bug on branch and stash without HEAD (#44)

--- pkg/git/cmd-stash.go
+++ pkg/git/cmd-stash.go
@@ -36,7 +36,8 @@ func (e *RepoEntity) loadStashedItems() error {
 	output := stashGet(e, "list")
 	stashIDRegex := regexp.MustCompile(`stash@{[\d]+}:`)
 	stashIDRegexInt := regexp.MustCompile(`[\d]+`)
-	stashBranchRegex := regexp.MustCompile(`[\w]+: `)
+	stashBranchRegex := regexp.MustCompile(`^(.*?): `)
+	stashMsgRegex := regexp.MustCompile(`WIP on \(?([^)]*)\)?`)
 	stashHashRegex := regexp.MustCompile(`[\w]{7}`)
 
 	stashlist := strings.Split(output, "\n")
@@ -55,6 +56,11 @@ func (e *RepoEntity) loadStashedItems() error {
 		// find branch
 		stashBranchRegexMatch := stashBranchRegex.FindString(trimmed)
 		branchName := stashBranchRegexMatch[:len(stashBranchRegexMatch)-2]
+
+		branchMatches := stashMsgRegex.FindStringSubmatch(branchName)
+		if len(branchMatches) >= 2 {
+			branchName = stashBranchRegexMatch[:len(stashBranchRegexMatch)-2]
+		}
 
 		// trim branch section
 		trimmed = stashBranchRegex.Split(trimmed, 2)[1]
