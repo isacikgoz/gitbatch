3dfbb270bdefa66f7bae32074be5b1c6d2eae686
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-10-07 19:02:45 +0300

    fix a race condition

--- internal/git/random.go
+++ internal/git/random.go
@@ -5,11 +5,10 @@ import (
 	"time"
 )
 
-var characterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-var r = rand.New(rand.NewSource(time.Now().UnixNano()))
-
 // RandomString generates a random string of n length
 func RandomString(n int) string {
+	var characterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = characterRunes[r.Intn(len(characterRunes))]
